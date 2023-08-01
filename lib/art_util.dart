7f6f3fc49c9e2f11924fca7ad8932702b562c4fb
bayram33 <s.bayram.1037@gmail.com>
2023-08-01 14:39:41 +0300

    navigate

---
+++ lib/art_util.dart
@@ -0,0 +1,15 @@
+class ArtUtil {
+  static const carava = "Caravaggıo";
+  static const monet = "Monet";
+  static const vangogh = "Van Gogh";
+
+  static const imgCarava =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Crucifixion_of_Saint_Peter-Caravaggio_%28c.1600%29.jpg/220px-Crucifixion_of_Saint_Peter-Caravaggio_%28c.1600%29.jpg";
+  static const imgMonet =
+      "https://upload.wikimedia.org/wikipedia/commons/a/a4/Claude_Monet_1899_Nadar_crop.jpg";
+  static const imgVanGogh =
+      "https://upload.wikimedia.org/wikipedia/commons/4/4c/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project_%28454045%29.jpg";
+
+  static const List<String> menuItems = [carava, monet, vangogh];
+  static const List<String> imgPath = [imgCarava, imgMonet, imgVanGogh];
+}
